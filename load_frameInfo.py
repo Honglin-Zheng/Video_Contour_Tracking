7ee085cb779ed09deae7e75cee3c7fc54d2f5def
Honglin-Zheng <colinzheng0418@gmail.com>
2018-03-07 22:05:26 -0800

    add option

--- load_frameInfo.py
+++ load_frameInfo.py
@@ -1,9 +1,11 @@
 import scipy.io as sio
 import matlab.engine
 
-def loadInfo(filename):
-  eng = matlab.engine.start_matlab()
-  eng.MotionBasedMultiObjectTrackingExample(filename, nargout=0)
+def loadInfo(filename, option=2):
+  if option == 1:
+    eng = matlab.engine.start_matlab()
+    eng.MotionBasedMultiObjectTrackingExample(filename, nargout=0)
+
   mat_contents = sio.loadmat('frameInfo.mat')
   frameInfo = mat_contents['frameInfo'][0]
   frameInfoList = []
