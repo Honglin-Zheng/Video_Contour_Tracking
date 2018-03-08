afdfb59de922466633aaf381963ce60fd29d9728
Honglin-Zheng <colinzheng0418@gmail.com>
2018-03-07 22:00:27 -0800

    Add tracking files and videos

---
+++ load_frameInfo.py
@@ -0,0 +1,15 @@
+import scipy.io as sio
+import matlab.engine
+
+def loadInfo(filename):
+  eng = matlab.engine.start_matlab()
+  eng.MotionBasedMultiObjectTrackingExample(filename, nargout=0)
+  mat_contents = sio.loadmat('frameInfo.mat')
+  frameInfo = mat_contents['frameInfo'][0]
+  frameInfoList = []
+  for i in range(len(frameInfo)):
+    if len(frameInfo[i]) == 0:
+      frameInfoList.append(None)
+    else:
+      frameInfoList.append(frameInfo[i][1])
+  return frameInfoList
